4740d082470913ec4788f6b958ac9e55bcda65fa
whyg0ng <dugustn2002@gmail.com>
2025-11-19 22:30:47 +0900

    whatever

--- week5/Project/src/apis/getUsers.js
+++ week5/Project/src/apis/getUsers.js
@@ -1,16 +1,81 @@
-const { notion } = require("../utils/connectNotionClient");
-
-async function getUsers(){
-    console.log("Getting Users...")
-    const response = await notion.users.list();
-    console.log("Users: ", response);
-
-    let users = [];
-    for(i=0; i<response.results.length; i++){
-        users.push({"id":response.results[i].id, "name":response.results[i].name});
-    }
-    console.log("Retrieved users: ",users)
-    return users;
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9a66ffac",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import os\n",
+    "import glob\n",
+    "from dotenv import load_dotenv\n",
+    "import gradio as gr\n",
+    "import json"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c424b79b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5c30f2f4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6d3a895e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6815d24e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8790ca72",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e28fed80",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e93c89cb",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
 }
-
-getUsers();
